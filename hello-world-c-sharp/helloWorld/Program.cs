0d9edfd50b5713327a77a82ca20aaf984f358bd6
u0114698 <marcus.kotze@kuleuven.be>
2021-06-01 21:45:15 +0200

    Initial commit of csharp and C hello-world

---
+++ hello-world-c-sharp/helloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace helloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
